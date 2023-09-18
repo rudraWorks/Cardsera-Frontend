2802caf45512c9f6535554fbe026e0f4fa2fbd51
Rudra Pratap Singh <merudra.official@gmail.com>
2023-09-19 02:21:52 +0530

    add linegraph

--- src/utils/dateFormatChanger.js
+++ src/utils/dateFormatChanger.js
@@ -1,10 +1,10 @@
 
 const formatChange = (ddmmyyyy) => {
     const arr = ddmmyyyy.split('/')
-    let day = arr[0]
+    let day = arr[1]
     if(day.length===1)
         day="0"+day
-    let month = arr[1]
+    let month = arr[0]
     if(month.length===1)
         month="0"+month
     const year = arr[2]
