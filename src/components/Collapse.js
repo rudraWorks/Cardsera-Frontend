b3956f434aaa80e9603bc90ba7f97ab0853f61d9
Rudra Pratap Singh <merudra.official@gmail.com>
2023-10-06 18:39:04 +0530

    update

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -10,7 +10,7 @@ const Wrapper = styled.div`
   margin: 10px;
   border: 1px solid #ccc;
   padding: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  //box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
 `;
 
